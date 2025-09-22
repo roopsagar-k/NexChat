e85c69a64e20f5a37d287bcfeb469c19a1afd146
roopsagar-k <roopsagarudayar9854@gmail.com>
2025-09-22 07:34:56 +0530

    cors update

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,7 +31,7 @@ app.get("/", (_req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
